eef3b0eca0467e97ae5366c8651eca2549772e5a
Borys3kk <szymon.banys01@wp.pl>
2022-03-02 14:48:30 +0100

    Project done

--- main.py
+++ main.py
@@ -10,18 +10,28 @@ WIN = pygame.display.set_mode(WIN_SIZE)
 pygame.display.set_caption("Planet Simulation")
 
 # physically accurate in order from the sun
+# mass of each planet in kg
 SUN_MASS = 1.989e30
 MERCURY_MASS = 3.285e23
 VENUS_MASS = 4.867e24
 EARTH_MASS = 5.972e24
 MARS_MASS = 6.39e23
+MOON_MASS = 7.342*10e22
+
+# velocity of each planet in m/s
+MERCURY_VELOCITY = 47.36e3
+VENUS_VELOCITY = 35.02e3
+EARTH_VELOCITY = 29.78e3
+MARS_VELOCITY = 24.1e3
+MOON_VELOCITY = 1.022e3
 
 # colors in rgb
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 BLUE = (100, 149, 237)
 RED = (188, 39, 50)
-DARK_GREY = (80, 78, 81)
+LIGHT_GRAY = (136, 136, 136)
+DARK_GRAY = (80, 78, 81)
 
 
 def main():
@@ -31,20 +41,34 @@ def main():
     # planets
     sun = Planet(0, 0, 30, YELLOW, SUN_MASS)
     sun.sun = True
-    mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, MERCURY_MASS)
-    venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, VENUS_MASS)
-    earth = Planet(-1 * Planet.AU, 0, 16, BLUE, EARTH_MASS)
-    mars = Planet(-1.524 * Planet.AU, 0, 12, RED, MARS_MASS)
-    planets = [sun, mercury, venus, earth, mars]
 
+    mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GRAY, MERCURY_MASS)
+    mercury.y_vel = -MERCURY_VELOCITY
+
+    venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, VENUS_MASS)
+    venus.y_vel = -VENUS_VELOCITY
+
+    earth = Planet(-1 * Planet.AU, 0, 16, BLUE, EARTH_MASS)
+    earth.earth = True
+    earth.y_vel = EARTH_VELOCITY
+
+    mars = Planet(-1.524 * Planet.AU, 0, 12, RED, MARS_MASS)
+    mars.y_vel = MARS_VELOCITY
+    #
+    # moon = Planet(-1 * Planet.AU, 10, 10, LIGHT_GRAY, MOON_MASS)
+    # moon.y_vel = MOON_VELOCITY
+
+    planets = [sun, mercury, venus, earth, mars]
     while run:
         clock.tick(60)  # limited to "60" Hz
-        pygame.display.update()
+        WIN.fill((0, 0, 0))
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
         for planet in planets:
+            planet.update_position(planets)
             planet.draw(WIN)
         pygame.display.update()
     pygame.quit()
--- planet.py
+++ planet.py
@@ -1,10 +1,16 @@
+import math
+
 import pygame
+
+pygame.init()
+FONT = pygame.font.SysFont("comicsans", 16)
+WHITE = (255, 255, 255)
 
 
 class Planet:
     AU = 149.6e6 * 1000  # astronomical unit
     G = 6.67428e-11
-    SCALE = 220 / AU
+    SCALE = 200 / AU
     TIMESTEP = 3600 * 24  # 1 day in seconds
     WIDTH = HEIGHT = 800
 
@@ -17,6 +23,7 @@ class Planet:
 
         self.orbit = []
         self.sun = False
+        self.earth = False
         self.distance_to_sun = 0
 
         self.x_vel = 0
@@ -25,4 +32,48 @@ class Planet:
     def draw(self, win):
         x = self.x * self.SCALE + self.WIDTH // 2
         y = self.y * self.SCALE + self.HEIGHT // 2
+
+        if len(self.orbit) > 2:
+            updated_points = []
+            for point in self.orbit:
+                _x, _y = point
+                _x = _x * self.SCALE + self.WIDTH // 2
+                _y = _y * self.SCALE + self.HEIGHT // 2
+                updated_points.append((_x, _y))
+
+            pygame.draw.lines(win, self.color, False, updated_points)
         pygame.draw.circle(win, self.color, (x, y), self.radius)
+        if not self.sun:
+            distance_text = FONT.render(f"{round(self.distance_to_sun / 1000, 1)}km", 1, WHITE)
+            win.blit(distance_text, (x - distance_text.get_width() / 2, y))
+
+    def attraction(self, other):
+        other_x, other_y = other.x, other.y
+        distance_x = other_x - self.x
+        distance_y = other_y - self.y
+        distance = math.sqrt(distance_x ** 2 + distance_y ** 2)
+
+        if other.sun:
+            self.distance_to_sun = distance
+
+        force = self.G * self.mass * other.mass / distance ** 2
+        theta = math.atan2(distance_y, distance_x)
+        force_x = math.cos(theta) * force
+        force_y = math.sin(theta) * force
+        return force_x, force_y
+
+    def update_position(self, planets):
+        total_fx = total_fy = 0
+        for planet in planets:
+            if self == planet:
+                continue
+            fx, fy = self.attraction(planet)
+            total_fx += fx
+            total_fy += fy
+
+        self.x_vel += total_fx / self.mass * self.TIMESTEP
+        self.y_vel += total_fy / self.mass * self.TIMESTEP
+
+        self.x += self.x_vel * self.TIMESTEP
+        self.y += self.y_vel * self.TIMESTEP
+        self.orbit.append((self.x, self.y))
